6585c9b4214efc1acf83d758e47aa6bb062a04f3
iamsuhail <s.muhammadsuhail123@gmail.com>
2024-06-09 22:54:44 +0530

    Updated file-tree-structure

--- FileManagementAPI/Controllers/FolderController.cs
+++ FileManagementAPI/Controllers/FolderController.cs
@@ -18,18 +18,73 @@ namespace FileManagementAPI.Controllers
 
         [HttpPost]
         [Route("CreateFolder")]
-        public async Task<IActionResult> CreateFolder(string folderName)
+        public async Task<IActionResult> CreateFolder(string folderName, string parentfolderName)
         {
-            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", folderName);
+            var baseDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", "Documents");
+            // baseDirectory = Path.Combine(baseDirectory, parentfolderName);
+            var folderPath = Path.Combine(baseDirectory, folderName);
+            var parentFolderId = 0;
+            if (parentfolderName != "root")
+            {
+                var folderInDb = _context.FolderData
+                        .FirstOrDefault(f => f.Name == parentfolderName);
+
+                if (folderInDb == null)
+                {
+                    // Folder doesn't exist in the database, return an error
+                    return NotFound($"Folder '{parentfolderName}' not found.");
+                }
+
+                // Get the full path of the folder from the database
+                string folderFullPath = folderInDb.Path;
+                // string parentDirectoryPath = Directory.GetParent(folderFullPath).FullName;
+
+                // Combine the full path with the base directory
+                folderPath = Path.Combine(folderFullPath, folderName);
+                var parentfolder = _context.FolderData
+                                .Where(f => f.Name == parentfolderName)
+                                .Select(f => new { f.Id, f.Name })
+                                .FirstOrDefault();
+                if (parentfolder == null)
+                {
+                    return Ok("Parent FOlder Not found");
+                }
+                parentFolderId = parentfolder.Id;
+                // folderPath = Path.Combine(baseDirectory, parentfolderName);
+                // folderPath = Path.Combine(folderPath, folderName);
+            }
+
 
             if (!Directory.Exists(folderPath))
             {
                 Directory.CreateDirectory(folderPath);
-                return Ok($"Folder '{folderName}' created successfully.");
+                //var completePath = Path.Combine(folderPath, folderName);
+                var completePath = folderPath;
+                try
+                {
+
+                    var fileMetadata = new Folder
+                    {
+                        Name = folderName,
+                        Path = completePath,
+                        CreatedAt = DateTime.UtcNow,
+                        UpdatedAt = DateTime.UtcNow,
+                        ParentFolderId = parentFolderId
+                    };
+
+                    _context.FolderData.Add(fileMetadata);
+                    await _context.SaveChangesAsync();
+
+                    return Ok($"Folder '{folderName}' created successfully.");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+                }
             }
             else
             {
-                return BadRequest($"Folder '{folderName}' already exists.");
+                return BadRequest($"Folder '{folderPath}' already exists.");
             }
         }
 
